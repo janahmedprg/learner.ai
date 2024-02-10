8345e99bb5d51e60debb50e0989550d88a4d1d6a
janahmedprg <jan.ahmed.prg@gmail.com>
2024-02-10 14:57:47 -0500

    styles

--- components/QuizDone.js
+++ components/QuizDone.js
@@ -1,25 +1,20 @@
-
-import { Button, StyleSheet, Text, View } from 'react-native';
-import { quizResults, submitQuiz } from './quizData';
+import { Button, StyleSheet, Text, View } from "react-native";
+import { quizResults, submitQuiz } from "./quizData";
 export default function QuizDoneScreen(props) {
-    const quizId = props.route.params.quizId
-    
+  const quizId = props.route.params.quizId;
 
-    
-    
-    return (
-        <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-            <Text>Quiz Done</Text>
-            <Text>Results</Text>
-            <Text>{JSON.stringify(quizResults[quizId] , null, 2)}</Text>
-            <Button
-                title="Submit"
-                onPress={() => {
-                    submitQuiz(quizId)
-                    props.navigation.navigate("Home") //can replace with quiz results page (show graph)
-                }}
-            >
-            </Button>
-        </View>
-    );
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Quiz Done</Text>
+      <Text>Results</Text>
+      <Text>{JSON.stringify(quizResults[quizId], null, 2)}</Text>
+      <Button
+        title="Submit"
+        onPress={() => {
+          submitQuiz(quizId);
+          props.navigation.navigate("Home"); //can replace with quiz results page (show graph)
+        }}
+      ></Button>
+    </View>
+  );
 }
--- components/QuizStart.js
+++ components/QuizStart.js
@@ -1,21 +1,24 @@
 //@ts-check
 
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import React, { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import QuizDoneScreen from './QuizDone';
-import { saveQuizAnswer, submitQuiz } from './quizData';
-import Slider from '@react-native-community/slider';
-
+import QuizDoneScreen from "./QuizDone";
+import { saveQuizAnswer, submitQuiz } from "./quizData";
+import Slider from "@react-native-community/slider";
 
 const QuizStack = createStackNavigator();
 
-function RatingInput({onAnswerSelected}){
-
-  return (
-    <RatingInputSlider onAnswerSelected={onAnswerSelected} />
-  )
+function RatingInput({ onAnswerSelected }) {
+  return <RatingInputSlider onAnswerSelected={onAnswerSelected} />;
   // return (
   //   <View style={{display: "flex", flexDirection: "row"}}>
   //     {["1", "2", "3", "4", "5"].map((value) => {
@@ -30,117 +33,112 @@ function RatingInput({onAnswerSelected}){
   // )
 }
 
-function RatingInputSlider({onAnswerSelected}){
-  
+function RatingInputSlider({ onAnswerSelected }) {
   return (
     <Slider
       style={{
-        width: "90%", 
+        width: "90%",
         height: 60,
-        // transform: [{ scaleY: 2 }] 
+        // transform: [{ scaleY: 2 }]
       }}
-      minimumTrackTintColor="#6879D0"
+      minimumTrackTintColor="#990030"
       maximumTrackTintColor="#000000"
-      thumbTintColor="#6879D0"
+      thumbTintColor="#990030"
       minimumValue={1}
       maximumValue={5}
-      value = {3}
+      value={3}
       step={1}
       onValueChange={onAnswerSelected}
     />
-  )
+  );
 }
 
 function QuestionScreen(props) {
-  const questionNumber = props.route.params?.questionNumber || 1
-  const questionsData = props.route.params.questionsData
-  const quizId = props.route.params.quizId
+  const questionNumber = props.route.params?.questionNumber || 1;
+  const questionsData = props.route.params.questionsData;
+  const quizId = props.route.params.quizId;
 
+  const isLastQuestion = questionNumber == questionsData.questions.length;
 
-  const isLastQuestion = questionNumber == questionsData.questions.length
-
-  function nextScreen(){
+  function nextScreen() {
     if (isLastQuestion) {
-      props.navigation.navigate("QuizDone", {quizId: quizId})
-      return
+      props.navigation.navigate("QuizDone", { quizId: quizId });
+      return;
     }
-    props.navigation.navigate("Question" + (questionNumber+1), {
+    props.navigation.navigate("Question" + (questionNumber + 1), {
       questionNumber: questionNumber + 1,
       questionsData: questionsData,
-      quizId: quizId
-    })
+      quizId: quizId,
+    });
   }
 
+  const [answer, setAnswer] = useState(3);
 
-  const [answer, setAnswer] = useState(3)
-  
-  const answerText = questionsData.answers ? questionsData.answers[questionNumber-1] : {
-    1: "Unfamiliar",
-    2: "Mostly Unfamiliar",
-    3: "Somewhat Unfamiliar",
-    4: "Familiar",
-    5: "Very Familiar"
-  }
-  
+  const answerText = questionsData.answers
+    ? questionsData.answers[questionNumber - 1]
+    : {
+        1: "Unfamiliar",
+        2: "Mostly Unfamiliar",
+        3: "Somewhat Unfamiliar",
+        4: "Familiar",
+        5: "Very Familiar",
+      };
 
   return (
     <View style={styles.container}>
-      <Text style={{
-        fontSize: 20,
-        marginBottom: 20
-      }}>
+      <Text
+        style={{
+          fontSize: 20,
+          marginBottom: 20,
+        }}
+      >
         {questionsData.questions[questionNumber - 1]}
       </Text>
 
-      <RatingInput onAnswerSelected={(value) => setAnswer(value)}/>
+      <RatingInput onAnswerSelected={(value) => setAnswer(value)} />
 
+      <Text style={{ fontSize: 18 }}>{answerText[answer]}</Text>
+      <Text></Text>
 
-
-    <Text style={{fontSize: 18}}>{answerText[answer]}</Text>
-    <Text></Text>
-
-
-
-    <CustomButton 
-      title="Next"
-      onPress={()=>{
-
-        saveQuizAnswer(quizId, questionNumber, answer)
-        nextScreen()
-      }}
-    />
-      
-    </View>
-  )
-}
-
-function QuizIntro({navigation}) {
-  return (
-    <View style={styles.container}>
-      <Text style={{
-        fontSize: 20,
-        marginBottom: 20,
-        textAlign: "center",
-        width: "80%"
-      }}>
-        Rank how familiar you are with the upcoming topics
-      </Text>
       <CustomButton
-        title="Start"
+        title="Next"
         onPress={() => {
-          navigation.navigate("Question1")
+          saveQuizAnswer(quizId, questionNumber, answer);
+          nextScreen();
         }}
       />
     </View>
   );
 }
 
-function QuestionRouter({questionsData , quizId}) {
+function QuizIntro({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 20,
+          marginBottom: 20,
+          textAlign: "center",
+          width: "80%",
+        }}
+      >
+        Rank how familiar you are with the upcoming topics
+      </Text>
+      <CustomButton
+        title="Start"
+        onPress={() => {
+          navigation.navigate("Question1");
+        }}
+      />
+    </View>
+  );
+}
 
-  const numQuestions = questionsData.questions.length
-  const questionsNumbers = []
+function QuestionRouter({ questionsData, quizId }) {
+  const numQuestions = questionsData.questions.length;
+  const questionsNumbers = [];
   for (let i = 1; i <= numQuestions; i++) {
-    questionsNumbers.push(i)
+    questionsNumbers.push(i);
   }
 
   return (
@@ -151,38 +149,33 @@ function QuestionRouter({questionsData , quizId}) {
       }}
     >
       <QuizStack.Screen name="QuizIntro" component={QuizIntro} />
-      {questionsNumbers.map(number => (
-        <QuizStack.Screen key={number} 
-          name={"Question" + number}  // used in the name to differentiate the screens, really goes off questionNumber param
-          component={QuestionScreen} 
+      {questionsNumbers.map((number) => (
+        <QuizStack.Screen
+          key={number}
+          name={"Question" + number} // used in the name to differentiate the screens, really goes off questionNumber param
+          component={QuestionScreen}
           initialParams={{
             questionNumber: 1,
             questionsData: questionsData,
-            quizId: quizId
+            quizId: quizId,
           }}
         />
       ))}
       <QuizStack.Screen name="QuizDone" component={QuizDoneScreen} />
     </QuizStack.Navigator>
-  )
+  );
 }
 
 export function Quiz(props) {
-  const {questionsData, quizId} = props.route.params
-  return (
-    <QuestionRouter
-      questionsData={questionsData}
-      quizId={quizId}
-    />
-  )
+  const { questionsData, quizId } = props.route.params;
+  return <QuestionRouter questionsData={questionsData} quizId={quizId} />;
 }
 
 //
 
 //
 
-export default function QuizStart({navigation}) {
-
+export default function QuizStart({ navigation }) {
   //todo: fetch tis from the backend based on quiz code
   const x = {
     1: "Unfamiliar",
@@ -190,14 +183,14 @@ export default function QuizStart({navigation}) {
     3: "Somewhat Unfamiliar",
     4: "Familiar",
     5: "Very Familiar",
-  }
+  };
   const y = {
     1: "Disagree",
     2: "Somewhat Disagree",
     3: "Neither Disagree nor Agree",
     4: "Somewhat Agree",
     5: "Agree",
-  }
+  };
   const sampleQuestionsData = {
     questions: [
       "How satisfied are you?",
@@ -206,72 +199,75 @@ export default function QuizStart({navigation}) {
       "Fourth Question",
       "Last Question",
     ],
-    answers: [
-      x,x,x,x,y
-    ],
-    quizId: "123"
-  }
-  const quizId = sampleQuestionsData.quizId //todo get rid of quizId prop and just use one in questionsdata
+    answers: [x, x, x, x, y],
+    quizId: "123",
+  };
+  const quizId = sampleQuestionsData.quizId; //todo get rid of quizId prop and just use one in questionsdata
 
-
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
       <TextInput
-        placeholder='Enter Quiz Code'
+        placeholder="Enter Quiz Code"
         maxLength={4}
-        onChangeText={text => setText(text)}
-        onSubmitEditing={()=>{
-          navigation.navigate("Quiz", {questionsData: sampleQuestionsData, quizId: text})
+        onChangeText={(text) => setText(text)}
+        onSubmitEditing={() => {
+          navigation.navigate("Quiz", {
+            questionsData: sampleQuestionsData,
+            quizId: text,
+          });
         }}
         style={{
           fontSize: 24,
-        borderWidth: 1,
-        borderColor: '#000',
-        paddingHorizontal: 10,
-        paddingVertical: 5,
-        width: '80%',
-        textAlign: 'center',
+          borderWidth: 1,
+          borderColor: "#000",
+          paddingHorizontal: 10,
+          paddingVertical: 5,
+          width: "80%",
+          textAlign: "center",
         }}
         value={text}
       ></TextInput>
       <Text></Text>
-      
-      <CustomButton 
+
+      <CustomButton
         title="Next"
-        onPress={()=>{
-          navigation.navigate("Quiz", {questionsData: sampleQuestionsData, quizId: quizId})
+        onPress={() => {
+          navigation.navigate("Quiz", {
+            questionsData: sampleQuestionsData,
+            quizId: quizId,
+          });
         }}
       />
     </View>
   );
 }
 
-function CustomButton({title, onPress}) {
+function CustomButton({ title, onPress }) {
   return (
     <TouchableOpacity
-        // title="Next"
-        style={{
-          width: 200,
-          height: 50,
-          backgroundColor: "#6879D0",
-          justifyContent: "center",
-          alignItems: "center",
-          borderRadius: 10,
-          marginBottom: 10,
-        }}
-        onPress={() => onPress()}
-      >
-        <Text style={{ color: "white", fontSize: 20 }}>{title}</Text>
-      </TouchableOpacity>
-  )
+      // title="Next"
+      style={{
+        width: 200,
+        height: 50,
+        backgroundColor: "#990030",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 10,
+        marginBottom: 10,
+      }}
+      onPress={() => onPress()}
+    >
+      <Text style={{ color: "white", fontSize: 20 }}>{title}</Text>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
